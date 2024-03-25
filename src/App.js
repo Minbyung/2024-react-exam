18452987889f1865e75a04b2b48226f01cb90303
Minbyung <mbmcybery@gmail.com>
2024-03-25 21:20:25 +0900

    useEffect 활용

--- src/App.js
+++ src/App.js
@@ -1,65 +1,57 @@
-import React, { useState, useRef } from 'react';
-import classnames from 'classnames';
+import React, { useState, useRef, useEffect } from 'react';
 
-let NotifyOnce_workDone = false;
+let AppCallCount = 0;
+let SubCallCount = 0;
 
+function Sub({appNo}) {
+  SubCallCount++;
+  const [no, setNo] = useState(0);
+  const [no2, setNo2] = useState(0);
 
-function NorifyOnce({children}) {
-  const [visible, setVisible] = useState(false);
- 
-  if ( NotifyOnce_workDone == false ) {
-    setTimeout(function() {
-      setVisible(true);
-    }, 1000);
+  useEffect(() => {
+    console.log('effect 1 : 한 번만 실행 ');
+  }, []);
 
-    setTimeout(function() {
-      setVisible(false);
-    }, 3000);
+  useEffect(() => {
+    console.log('effect 2 : 부모(App)이 no가 바뀔 때 마다 실행');
+  }, [appNo]);
 
+  useEffect(() => {
+    console.log('effect 3 : 나(Sub)의 no가 바뀔 때 마다 실행 ');
+  }, [no, no2]);
 
-    NotifyOnce_workDone = true;
-  }
+  useEffect(() => {
+    console.log('effect 4 : appNo, no가 바뀔 때 마다 실행 ');
+  }, [appNo, no, no2]);
 
-  return (
-    <div
-      className={classnames(
-        "fixed transition-all right-[10px]",
-        {
-          "top-[-60px]" : !visible
-        },
-        {
-          "top-[5px]" : visible
-        }
-      )}
-    >
-      {children}
-    </div>
-  )
-}
+  useEffect(() => {
+    console.log('effect 5 : 매번 실행');
+  });
 
-
-function Alert({color:color_, children}) {
-  const color = color_ ?? "white";
-  
-  return ( 
-    <div role="alert" className="alert alert-info">
-      <div className={`text-[${color}]`}>
-        <i className="fa-solid fa-face-smile"></i>
-        <span>{children}</span>
-      </div>
-    </div>
-  )
-}
-    
-export default function App() {
   return (
     <>
-      <NorifyOnce>
-        <Alert>안녕</Alert>
-      </NorifyOnce>
-      
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci corporis ratione voluptates nostrum aliquid quis magnam ab voluptatum rerum autem iste, sequi corrupti, assumenda modi voluptatibus tenetur perferendis ducimus?
+      <div className='p-10 border border-blue-500'>
+        AppNo : {appNo}
+        <hr />
+        <button className='btn' onClick={() => setNo(no + 1)}>sub 증가 : {no}</button>
+        <button className='btn' onClick={() => setNo2(no + 1)}>sub2 증가 : {no2}</button>
+        
+      </div>
+    
+    </>
+  )
+}
+
+export default function App() {
+  AppCallCount++;
+  const [no, setNo] = useState(0);
+
+  return (
+    <>
+      <div className='p-10 border border-red-500'>
+        <button className='btn' onClick={() => setNo(no + 1)}>app 증가 : {no}</button>
+        <hr />
+        <Sub appNo={no}/>
       </div>
     </>
   );
