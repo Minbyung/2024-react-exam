b40b0b69d47002c91c037931ed91bdbf1dbb122c
Minbyung <mbmcybery@gmail.com>
2024-03-27 21:11:03 +0900

    체크박스 다루기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const fruits = ['사과', '바나나', '귤', '코코넛'];
+  const [selecteds, setSelecteds] = useState(new Array(fruits.length).fill(true));
 
-  const handleClick = useCallback(() => setCount(count + 1), [count]);
+  const toggleFruitSelected = (index) => {
+    const newSelecateds = selecteds.map((el, _index) => _index == index ? !el : el);
+    setSelecteds(newSelecateds);
+  }
+
+  const selectedsFruits = selecteds.map((el, index) => el ? fruits[index] : el).filter(el => el);
 
   return (
     <>
+    <ul>
+      {fruits.map((fruit, index) => (
+        <li key={index}>
+          <label>
+            <input checked={selecteds[index]} type="checkbox" onChange={() => toggleFruitSelected(index)} />
+            {fruit}
+          </label>
+        </li>
+      ))}
+    </ul>
+
+
+
     <div>
-      <span>클릭 횟수 : {count}</span>
-      <button onClick={handleClick}>클릭</button>
+      선택 상태 : {selecteds.join(', ')}
+      <hr />
+      선택된 과일 : {selectedsFruits.join(', ')}
     </div>
     </>
   );
