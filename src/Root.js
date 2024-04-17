fb9e6db5dccc37c8b575081194d5dfc85cde8d6d
Minbyung <mbmcybery@gmail.com>
2024-04-17 19:47:05 +0900

    리코일 적용

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import {ThemeProvider, CssBaseline, createTheme} from "@mui/material";
 import App from './App';
+import { RecoilRoot } from "recoil";
 
 export default function Root() {
   const muiThemePaletteKeys = [
@@ -48,7 +49,9 @@ export default function Root() {
     return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
       </ThemeProvider>
     );
   }
